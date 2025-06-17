2a9ad6a945de734f8b6e3e885b9f6cacefed9f91
evanqhu <qianghu98@163.com>
2025-06-17 17:14:10 +0800

    feat: update

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -9,5 +9,20 @@ export default defineClientConfig({
   enhance({ app }) {
     app.component('RepoCard', RepoCard)
     // app.component('CustomComponent', CustomComponent)
+    
+    // 打印简历时，修改标题
+    if (typeof window !== 'undefined') {
+      // 保存原始标题
+      let originalTitle = '';
+
+      window.addEventListener('message', (event) => {
+        if (event.data.type === 'SET_PRINT_TITLE') {
+          originalTitle = document.title;
+          document.title = event.data.title;
+        } else if (event.data.type === 'RESTORE_TITLE') {
+          document.title = originalTitle;
+        }
+      });
+    }
   },
 })
